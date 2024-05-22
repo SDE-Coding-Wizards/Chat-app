0a30ed321b9e7f80c3c93d82ed54f3f0ac5512cb
half0150 <half0150@edu.sde.dk>
2024-05-22 10:35:32 +0200

    Navbar font size changed, and login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-slate-950">
       <div className="flex space-x-4">
+        <Link href="">
         <button className="px-4 py-2 bg-violet-500 text-black text-xl font-bold rounded-lg">Login</button>
+        </Link>
+        <Link href="/signUp">
         <button className="px-4 py-2 bg-violet-500 text-black text-xl font-bold rounded-lg">Sign Up</button>
+        </Link>
       </div>
     </main>
   );
