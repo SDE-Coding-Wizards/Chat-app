c191c0506a9fdf042f778bd68a9ba16043241880
half0150 <half0150@edu.sde.dk>
2024-05-23 13:17:16 +0200

    api

--- app/(pages)/signUp/page.tsx
+++ app/(pages)/signUp/page.tsx
@@ -3,11 +3,12 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function login() {
+
+export default function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  async function handleLogin() {
-    const response = await axios.post("/api/login", {
+  async function handleSignup() {
+    const response = await axios.post("/api/auth/signup", {
       email,
       password,
     });
@@ -15,9 +16,14 @@ export default function login() {
     console.log(response.data);
 
   }
+
   return (
     <main className="flex items-center justify-center min-h-screen bg-base-100">
       <div className="flex flex-col space-y-4">
+        <label className="input input-bordered flex items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+          <input type="text" className="grow" placeholder="Username" />
+        </label>
         <label className="input input-bordered flex items-center gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
           <input type="text" className="grow" placeholder="Email" onChange={(event) => {
@@ -57,8 +63,8 @@ export default function login() {
           </div>
           Sign in with Google
         </button>
-        <button className="px-4 py-2 btn btn-neutral text-xl font-bold rounded-lg" onClick={handleLogin}>
-          Sign In
+        <button className="px-4 py-2 btn btn-neutral text-xl font-bold rounded-lg" onClick={handleSignup}>
+          Sign Up
         </button>
       </div>
     </main>
