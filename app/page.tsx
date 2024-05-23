582aac5fdeb9b74bfe150d74d588560eecbcc08a
half0150 <half0150@edu.sde.dk>
2024-05-23 08:27:13 +0200

    Chat page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex items-center justify-center min-h-screen bg-base-100">
       <div className="flex space-x-4">
         <Link href="/login">
-          <button className="px-4 py-2 btn btn-neutral text-xl font-bold rounded-lg">Login</button>
+          <button className="px-4 py-2 btn btn-neutral text-xl text font-bold rounded-lg">Login</button>
         </Link>
         <Link href="/signUp">
           <button className="px-4 py-2 btn btn-neutral text-xl font-bold rounded-lg">Sign Up</button>
