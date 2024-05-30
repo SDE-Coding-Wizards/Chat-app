8027c4a584cfe0b434f5a991af18c03cb967c644
half0150 <half0150@edu.sde.dk>
2024-05-30 15:08:00 +0200

    Theme Controller added to navbar

--- app/(pages)/profileSettings/page.tsx
+++ app/(pages)/profileSettings/page.tsx
@@ -1,6 +1,4 @@
 export default function profileSettings() {
-
-
     return (
         <div className="text-center mt-5">
             <div className="flex justify-center items-center">
@@ -10,31 +8,30 @@ export default function profileSettings() {
                         alt="Profile Picture"
                         className="rounded-full w-32 h-32"
                     />
-                    <button className="btn btn-neutral mt-2">Change Picture</button>
+                    <button className="btn btn-neutral mt-3">Change Picture</button>
                 </div>
-                <div className="flex flex-col items-start ml-5">
-                    <label htmlFor="username" className="text-lg">Username</label>
-                    <input
-                        type="text"
-                        id="username"
-                        className="input input-primary w-64"
-                        placeholder="Username"
-                    />
-                    <label htmlFor="email" className="text-lg mt-2">Email</label>
+                <div className="flex flex-col items-start ml-5 mt-5">
+                    <label className="text-lg mt-5">Email</label>
                     <input
                         type="email"
                         id="email"
                         className="input input-primary w-64"
                         placeholder="Email"
                     />
-                    <label htmlFor="password" className="text-lg mt-2">Password</label>
+                    <label className="text-lg mt-5">Password</label>
                     <input
                         type="password"
                         id="password"
                         className="input input-primary w-64"
                         placeholder="Password"
                     />
-                    <button className="btn mt-2">Save</button>
+                    <label className="text-lg mt-5">Bio</label>
+                    <textarea
+                        id="bio"
+                        className="input input-primary w-64"
+                        placeholder="Tell us about yourself"
+                    />
+                    <button className="btn mt-7 w-64">Save & Update</button>
                 </div>
             </div>
         </div>
