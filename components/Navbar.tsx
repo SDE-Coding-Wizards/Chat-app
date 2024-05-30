512674d539c19fab51c2615b239fec4ef04a0b2c
Nima Badiei <nimabadiei@gmail.com>
2024-05-30 23:58:12 +0200

    Navbar fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,13 @@ import ThemeController from "./ThemeController";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between h-20 bg-base-200 border-b-2 border-base-300">
+    <nav className="flex items-center justify-between h-16 bg-base-200 border-b-2 border-base-300 relative">
       <div className="flex items-center justify-center w-full">
         <Link href="/" className="font-bold text-3xl rounded-md">
           Chat Wizards
         </Link>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center absolute right-6">
         <ThemeController />
       </div>
     </nav>
