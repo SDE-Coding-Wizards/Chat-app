5763c7232ed03057b9336808292dc286d1ff9744
Ikhvan <ikhvan95@gmail>
2024-06-06 10:07:29 +0200

    Now whenever you go to our website you'll go to login page first

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true, // Set to true if this redirect is permanent
+      },
+    ];
+  },
 };
 
 export default nextConfig;
