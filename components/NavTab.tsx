ffe9127546365dd6439e7ee21bc05ba854bdbd93
half0150 <half0150@edu.sde.dk>
2024-06-06 10:19:15 +0200

    Fixed navTab so you now can press Online, all and pending

--- components/NavTab.tsx
+++ components/NavTab.tsx
@@ -1,56 +1,56 @@
-
 "use client";
 import { useState } from "react";
-import { useSearchParams } from "next/navigation";
 import Image from "next/image";
-import Link from "next/link";
 import AddFriend from "@/app/(pages)/friendTabs/AddFriend";
+import OnlineUsers from "@/app/(pages)/friendTabs/Online";
+import AllUsers from "@/app/(pages)/friendTabs/All";
+import PendingUsers from "@/app/(pages)/friendTabs/Pending";
 
 export default function NavTab() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search");
-
+  const [activeTab, setActiveTab] = useState(null);
   const [isAddFriendModalOpen, setIsAddFriendModalOpen] = useState(false);
 
   const openAddFriendModal = () => setIsAddFriendModalOpen(true);
-  const closeAddfriendModal = () => setIsAddFriendModalOpen(false);
+  const closeAddFriendModal = () => setIsAddFriendModalOpen(false);
+
+  const handleTabClick = (tab) => {
+    if (activeTab === tab) {
+      setActiveTab(null); // Toggle active tab if clicked again
+    } else {
+      setActiveTab(tab);
+    }
+  };
 
   return (
-    <nav className="bg-base-200 w-full p-4 flex justify-end items-center shadow-lg">
-      <Link href={`/?search=online`} passHref>
+    <>
+      <nav className="bg-base-200 w-full p-4 flex justify-end items-center shadow-lg">
         <span
-          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${
-            search === "online" && "bg-green-400"
-          }`}
+          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${activeTab === "Online" && "bg-green-400"
+            }`}
+          onClick={() => handleTabClick("Online")}
         >
           Online
         </span>
-      </Link>
-      <Link href={`/?search=all`} passHref>
         <span
-          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${
-            search === "all" && "bg-green-400"
-          }`}
+          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${activeTab === "All" && "bg-green-400"
+            }`}
+          onClick={() => handleTabClick("All")}
         >
           All
         </span>
-      </Link>
-      <Link href={`/?search=pending`} passHref>
         <span
-          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${
-            search === "pending" && "bg-green-400"
-          }`}
+          className={`p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105 ${activeTab === "Pending" && "bg-green-400"
+            }`}
+          onClick={() => handleTabClick("Pending")}
         >
           Pending
         </span>
-      </Link>
-      <button
-        onClick={openAddFriendModal}
-        className="p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105"
-      >
-        Add Friend
-      </button>
-      <Link href="/profileSettings" passHref>
+        <button
+          onClick={openAddFriendModal}
+          className="p-2 mx-2 bg-base-300 rounded-md transition duration-300 ease-in-out transform hover:bg-base-400 active:bg-base-500 hover:scale-105"
+        >
+          Add Friend
+        </button>
         <span>
           <Image
             className="rounded-3xl mx-2"
@@ -60,8 +60,13 @@ export default function NavTab() {
             height={50}
           />
         </span>
-      </Link>
-      <AddFriend isOpen={isAddFriendModalOpen} onClose={closeAddfriendModal} />
-    </nav>
+        <AddFriend isOpen={isAddFriendModalOpen} onClose={closeAddFriendModal} />
+      </nav>
+      <div className="p-4">
+        {activeTab === "Online" && <OnlineUsers />}
+        {activeTab === "All" && <AllUsers />}
+        {activeTab === "Pending" && <PendingUsers />}
+      </div>
+    </>
   );
 }
