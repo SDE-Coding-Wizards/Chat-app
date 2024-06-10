fe7d36f77965cf542dab5deb144a0277b4171cfa
Philip Guldborg <phgu03@gmail.com>
2024-06-10 22:04:16 +0200

    🐞 fix(secrets): Nextjs compatible

--- firebase.ts
+++ firebase.ts
@@ -1,15 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import getConfig from "next/config";
+
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY || "",
-  authDomain: process.env.FIREBASE_AUTHDOMAIN || "",
-  projectId: process.env.FIREBASE_PROJECTID || "",
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET || "",
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID || "",
-  appId: process.env.FIREBASE_APPID || "",
-  measurementId: process.env.FIREBASE_MEASUREMENTID || ""
+  apiKey: serverRuntimeConfig.FIREBASE_APIKEY || publicRuntimeConfig.FIREBASE_APIKEY,
+  authDomain: serverRuntimeConfig.FIREBASE_AUTHDOMAIN || publicRuntimeConfig.FIREBASE_AUTHDOMAIN,
+  projectId: serverRuntimeConfig.FIREBASE_PROJECTID || publicRuntimeConfig.FIREBASE_PROJECTID,
+  storageBucket: serverRuntimeConfig.FIREBASE_STORAGEBUCKET || publicRuntimeConfig.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: serverRuntimeConfig.FIREBASE_MESSAGINGSENDERID || publicRuntimeConfig.FIREBASE_MESSAGINGSENDERID,
+  appId: serverRuntimeConfig.FIREBASE_APPID || publicRuntimeConfig.FIREBASE_APPID,
+  measurementId: serverRuntimeConfig.FIREBASE_MEASUREMENTID || publicRuntimeConfig.FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverRuntimeConfig: {
+    apiKey: process.env.FIREBASE_APIKEY ?? "",
+    authDomain: process.env.FIREBASE_AUTHDOMAIN ?? "",
+    projectId: process.env.FIREBASE_PROJECTID ?? "",
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET ?? "",
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID ?? "",
+    appId: process.env.FIREBASE_APPID ?? "",
+    measurementId: process.env.FIREBASE_MEASUREMENTID ?? ""
+  },
+  publicRuntimeConfig: {
+    apiKey: process.env.FIREBASE_APIKEY ?? "",
+    authDomain: process.env.FIREBASE_AUTHDOMAIN ?? "",
+    projectId: process.env.FIREBASE_PROJECTID ?? "",
+    storageBucket: process.env.FIREBASE_STORAGEBUCKET ?? "",
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID ?? "",
+    appId: process.env.FIREBASE_APPID ?? "",
+    measurementId: process.env.FIREBASE_MEASUREMENTID ?? ""
+  },
   reactStrictMode: false,
   images: {
     remotePatterns: [
